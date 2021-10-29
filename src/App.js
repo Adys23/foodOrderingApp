7d7202980170a549f2fb02c040423167da29d6cf
Adrian W <wasilewski.adrian.64@gmail.com>
2021-10-29 13:15:54 +0200

    cartContext created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 function App() {
 	const [cartVisible, setCartVisibility] = useState(false);
@@ -12,13 +13,13 @@ function App() {
 	};
 
 	return (
-		<>
+		<CartProvider>
 			{cartVisible && <Cart onHideCart={cartVisibilityHandler} />}
 			<Header onShowCart={cartVisibilityHandler} />
 			<main>
 				<Meals />
 			</main>
-		</>
+		</CartProvider>
 	);
 }
 
