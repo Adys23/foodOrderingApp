9a7d10479d359665096395eca66756d659c56b6d
Adrian W <wasilewski.adrian.64@gmail.com>
2021-11-03 21:12:18 +0100

    cartItem added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,32 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
+
 import Modal from '../UI/Modal/Modal';
+import CartItem from './CartItem/CartItem';
 
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
+	const cartCtx = useContext(CartContext);
+
+	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+	const hasItems = cartCtx.items.length > 0;
+
+	const cartItemRemoveHandler = (id) => {};
+
+	const cartItemAddHandler = (item) => {};
+
 	const cartItems = (
 		<ul className={classes['cart-items']}>
-			{[
-				{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 },
-				{ id: 'c2', name: 'Dumplings', amount: 3, price: 7.5 },
-				{ id: 'c3', name: 'Garlic bread', amount: 5, price: 6.0 },
-				{ id: 'c4', name: 'Pizza', amount: 1, price: 30.99 },
-				{ id: 'c5', name: 'Hamburger', amount: 1, price: 12.99 },
-				{ id: 'c6', name: `Burrito`, amount: 3, price: 7.99 },
-			].map((item) => (
-				<li>{item.name}</li>
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					price={item.price}
+					name={item.name}
+					amount={item.amount}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
 			))}
 		</ul>
 	);
@@ -21,17 +34,19 @@ const Cart = (props) => {
 	return (
 		<Modal onHide={props.onHideCart}>
 			{cartItems}
-			<div>
+			<div className={classes.total}>
 				<span>Total amount</span>
-				<span>33.33</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className={classes.actions}>
 				<button className={classes['button--alt']} onClick={props.onHideCart}>
 					Close
 				</button>
-				<button className={classes.button} onClick={props.onHideCart}>
-					Order
-				</button>
+				{hasItems && (
+					<button className={classes.button} onClick={props.onHideCart}>
+						Order
+					</button>
+				)}
 			</div>
 		</Modal>
 	);
